8708918c9dc5d1538476319e5de6c21c8ae50f63
cody.stoutenburg@gmail.com <cody.stoutenburg@gmail.com@9b2beeca-6fbb-a3e4-6c1f-7d7f8f7dc6a2>
2013-03-12 11:23:21 +0000

    Update element creation tu use some king of plugin, temporary version because hard coded, need to modify PanelCreateMapElement

--- CampaignPlayer/src/main/java/net/alteiar/campaign/player/gui/map/element/PanelCreateMapElement.java
+++ CampaignPlayer/src/main/java/net/alteiar/campaign/player/gui/map/element/PanelCreateMapElement.java
@@ -6,6 +6,12 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -40,23 +46,70 @@ public class PanelCreateMapElement extends JPanel implements PanelOkCancel {
 		}
 	}
 
-	private final PanelMapElementBuilder[] panelBuilder = new PanelMapElementBuilder[] {
-			new PanelCircleBuilder(), new PanelRectangleBuilder(),
-			new PanelCharacterBuilder() };
+	/*
+	 * private final PanelMapElementBuilder[] panelBuilder = new
+	 * PanelMapElementBuilder[] { new PanelCircleBuilder(), new
+	 * PanelRectangleBuilder(), new PanelCharacterBuilder() };
+	 */
 
-	private PanelMapElementBuilder builder;
+	public static PanelMapElementBuilder[] getBuilders() {
+		PanelMapElementBuilder[] res = null;
+		ClassLoader loader;
+		try {
+			loader = URLClassLoader.newInstance(new URL[] { new File(
+					"./ressources/plugin/Pathfinder-system-1.0-SNAPSHOT.jar")
+					.toURI().toURL() }, PanelCreateMapElement.class
+					.getClassLoader());
+			Class<?> clazz = Class.forName(
+					"pathfinder.gui.builder.PanelBuilder", true, loader);
+			Class<? extends IPanelBuilders> runClass = clazz
+					.asSubclass(IPanelBuilders.class);
+			// Avoid Class.newInstance, for it is evil.
+			Constructor<? extends IPanelBuilders> ctor = runClass
+					.getConstructor();
+			IPanelBuilders doRun = ctor.newInstance();
+			res = doRun.getBuilders();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NoSuchMethodException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return res;
+	}
 
 	private final JPanel panelWest;
 	private final JPanel panelCenter;
+	private PanelMapElementBuilder builder;
 
 	public PanelCreateMapElement() {
 		super(new BorderLayout());
 
-		panelWest = new JPanel(new GridLayout(panelBuilder.length, 1));
+		panelWest = new JPanel(new GridLayout(getBuilders().length, 1));
 
 		this.add(panelWest, BorderLayout.WEST);
 
-		builder = panelBuilder[0];
+		builder = getBuilders()[0];
 		panelCenter = new JPanel();
 		panelCenter.add(builder);
 		this.add(panelCenter, BorderLayout.CENTER);
@@ -66,7 +119,7 @@ public class PanelCreateMapElement extends JPanel implements PanelOkCancel {
 		panelWest.removeAll();
 		int maxWidth = 50;
 		int maxHeight = 50;
-		for (final PanelMapElementBuilder panel : panelBuilder) {
+		for (final PanelMapElementBuilder panel : getBuilders()) {
 			Dimension dim = panel.getPreferredSize();
 
 			maxWidth = Math.max(maxWidth, dim.width);
