9a420fee456ca3c687663ef22b7c7e2d03c32dde
GouletSyl@gmail.com <GouletSyl@gmail.com@9b2beeca-6fbb-a3e4-6c1f-7d7f8f7dc6a2>
2013-03-19 19:31:47 +0000

    Ajout d'authorisation pour les modifications de bean

---
+++ trunk/DistributedBeanManager/src/main/java/net/alteiar/client/ClientId.java
@@ -0,0 +1,19 @@
+package net.alteiar.client;
+
+public class ClientId {
+
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String toString() {
+		return Integer.toString(id);
+	}
+	
+}
