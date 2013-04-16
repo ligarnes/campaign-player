13f2ced2f9fb185422867dd0d0ee9de95cfe480e
kile_katarn@hotmail.com <kile_katarn@hotmail.com@9b2beeca-6fbb-a3e4-6c1f-7d7f8f7dc6a2>
2013-04-16 23:19:01 +0000

    Début d'implémentation d'ajout d'effet sur les MapElements du systeme

--- Effect-System/src/main/java/net/alteiar/effectBean/Effect.java
+++ Effect-System/src/main/java/net/alteiar/effectBean/Effect.java
@@ -1,5 +1,73 @@
 package net.alteiar.effectBean;
 
-public abstract class Effect implements MapElement{
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Point;
 
+import pathfinder.character.PathfinderCharacter;
+import pathfinder.mapElement.character.PathfinderCharacterElement;
+
+import net.alteiar.map.elements.CircleElement;
+import net.alteiar.map.elements.ColoredShape;
+import net.alteiar.map.elements.MapElement;
+import net.alteiar.shared.UniqueID;
+import net.alteiar.utils.map.element.MapElementSize;
+import net.alteiar.utils.map.element.MapElementSizeSquare;
+
+public abstract class Effect extends MapElement{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private Boolean oneUse;
+	private ColoredShape shape;
+	
+	
+	public Effect(Point position,Color color, Boolean oneUse) {
+		super(position);
+		shape=new CircleElement(position,color, new MapElementSizeSquare(1));
+		this.oneUse=false;
+	}
+	
+	public Effect(Point position, Boolean oneUse) {
+		super(position);
+		shape=new CircleElement(position,Color.red, new MapElementSizeSquare(1));
+		this.oneUse=false;
+	}
+	
+	public ColoredShape getShape()
+	{
+		return shape;
+	}
+	
+	public void setShape(ColoredShape shape)
+	{
+		this.shape=shape;
+		this.shape.setPosition(this.getPosition());
+	}
+	
+	
+	public void draw(Graphics2D g, double zoomFactor){
+		shape.draw(g, zoomFactor);
+	}
+
+	public Boolean contain(Point p){
+		return shape.contain(p);
+	}
+
+	public Double getWidthPixels(){
+		return shape.getWidthPixels();
+	}
+
+	public Double getHeightPixels(){
+		return shape.getHeightPixels();
+	}
+	
+	public void setMapId(UniqueID mapId)
+	{
+		super.setMapId(mapId);
+		shape.setMapId(mapId);
+	}
+	public abstract void activate(PathfinderCharacter c);
 }
---
+++ Effect-System/src/test/java/BasicTest.java
@@ -0,0 +1,87 @@
+
+
+import static org.junit.Assert.fail;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public abstract class BasicTest {
+
+	protected Boolean compareImage(BufferedImage img1, BufferedImage img2)
+			throws IOException {
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ImageIO.write(img1, "jpg", baos);
+		baos.flush();
+		byte[] targetBytes = baos.toByteArray();
+		baos.close();
+
+		baos = new ByteArrayOutputStream();
+		ImageIO.write(img2, "jpg", baos);
+		baos.flush();
+		byte[] resultBytes = baos.toByteArray();
+		baos.close();
+
+		return Arrays.equals(targetBytes, resultBytes);
+	}
+
+	protected void testImage(BufferedImage image, BufferedImage image2) {
+		JFrame frm = new JFrame();
+
+		JLabel lbl1 = new JLabel(new ImageIcon(image));
+		lbl1.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+		JLabel lbl2 = new JLabel(new ImageIcon(image2));
+		lbl2.setBorder(BorderFactory.createLineBorder(Color.black, 2));
+
+		frm.add(lbl1, BorderLayout.WEST);
+		frm.add(lbl2, BorderLayout.CENTER);
+		frm.pack();
+		frm.setVisible(true);
+
+		try {
+			Thread.sleep(1000000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	protected void sleep(long time) {
+		try {
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			fail("not able to sleep");
+		}
+	}
+
+	public String getCampaignName() {
+		return "general-test";
+	}
+
+	public final String getCampaignDirectory() {
+		return "./test/ressources/campaign/" + getCampaignName();
+	}
+
+	public String getPlayerName() {
+		return "player-name";
+	}
+
+	public static void deleteRecursive(File base) {
+		if (base.listFiles() != null) {
+			for (File f : base.listFiles()) {
+				deleteRecursive(f);
+			}
+		}
+		base.delete();
+	}
+}
