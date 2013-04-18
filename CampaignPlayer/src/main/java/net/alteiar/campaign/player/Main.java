86652552996aedae33a32ddbbb85c5b1685e1e3c
GouletSyl@gmail.com <GouletSyl@gmail.com@9b2beeca-6fbb-a3e4-6c1f-7d7f8f7dc6a2>
2013-04-18 22:25:04 +0000

    Update Start Menu --> Encore du travail à faire (et nettoyer le code!)

--- CampaignPlayer/src/main/java/net/alteiar/campaign/player/Main.java
+++ CampaignPlayer/src/main/java/net/alteiar/campaign/player/Main.java
@@ -19,16 +19,19 @@
  */
 package net.alteiar.campaign.player;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.io.IOException;
 import java.rmi.RMISecurityManager;
 
+import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
 import net.alteiar.CampaignClient;
 import net.alteiar.campaign.player.gui.MainFrame;
+import net.alteiar.campaign.player.gui.MainPanelStartGame;
 import net.alteiar.campaign.player.gui.PanelConnexion;
 import net.alteiar.dialog.DialogOkCancel;
 import net.alteiar.shared.ExceptionTool;
@@ -61,57 +64,58 @@ public class Main {
 		} catch (IllegalAccessException e) {
 			e.printStackTrace();
 		}
+		
+		JDialog enterDialog = new JDialog(MainFrame.FRAME, "Choix de la partie", true);
 
-		DialogOkCancel<PanelConnexion> dlg = new DialogOkCancel<PanelConnexion>(
-				MainFrame.FRAME, "Connexion", true, new PanelConnexion());
-
-		dlg.setLocationRelativeTo(null);
-		dlg.setOkText("Connexion");
-		dlg.setCancelText("Annuler");
-		dlg.setLayout(FlowLayout.CENTER);
-		dlg.setVisible(true);
-		if (dlg.getReturnStatus() == DialogOkCancel.RET_OK) {
-			GlobalProperties globalProp = Helpers.getGlobalProperties();
-			String localAdress = dlg.getMainPanel().getLocalAdressIP();
-			String address = dlg.getMainPanel().getServerAddressIp();
-			String port = dlg.getMainPanel().getPort();
-
-			String name = dlg.getMainPanel().getPseudo();
-			Boolean isMj = dlg.getMainPanel().isMj();
-
-			Boolean isServer = dlg.getMainPanel().isServer();
-
-			if (isServer) {
-				CampaignClient.startNewCampaignServer(localAdress, address,
-						port, "campaign path");
-			} else {
-				CampaignClient.connectToServer(localAdress, address, port,
-						"campaign path");
-			}
-			CampaignClient.getInstance().createPlayer(name, isMj);
-
-			globalProp.setPseudo(name);
-			globalProp.setIsMj(isMj);
-			globalProp.setIpLocal(localAdress);
-			globalProp.setPort(port);
-			globalProp.setIpServer(address);
-			globalProp.setIsServer(isServer);
-			try {
-				globalProp.save();
-			} catch (IOException ex) {
-				ExceptionTool.showError(ex);
-			}
-
-			SwingUtilities.invokeLater(new Runnable() {
-				@Override
-				public void run() {
-					MainFrame.FRAME.initComponent();
-					// CampaignClient.createClientCampaign(5, MainFrame.FRAME);
-					MainFrame.FRAME.setVisible(true);
-				}
-			});
-		} else {
-			System.exit(0);
-		}
+		enterDialog.getContentPane().add(new MainPanelStartGame());
+		
+		// TODO : ajuster la taille du panel
+		enterDialog.setPreferredSize(new Dimension(800, 600));
+		enterDialog.pack();
+		enterDialog.setLocationRelativeTo(null);
+		enterDialog.setVisible(true);
+//		if (dlg.getReturnStatus() == DialogOkCancel.RET_OK) {
+//			GlobalProperties globalProp = Helpers.getGlobalProperties();
+//			String localAdress = dlg.getMainPanel().getLocalAdressIP();
+//			String address = dlg.getMainPanel().getServerAddressIp();
+//			String port = dlg.getMainPanel().getPort();
+//
+//			String name = dlg.getMainPanel().getPseudo();
+//			Boolean isMj = dlg.getMainPanel().isMj();
+//
+//			Boolean isServer = dlg.getMainPanel().isServer();
+//
+//			if (isServer) {
+//				CampaignClient.startNewCampaignServer(localAdress, address,
+//						port, "campaign path");
+//			} else {
+//				CampaignClient.connectToServer(localAdress, address, port,
+//						"campaign path");
+//			}
+//			CampaignClient.getInstance().createPlayer(name, isMj);
+//
+//			globalProp.setPseudo(name);
+//			globalProp.setIsMj(isMj);
+//			globalProp.setIpLocal(localAdress);
+//			globalProp.setPort(port);
+//			globalProp.setIpServer(address);
+//			globalProp.setIsServer(isServer);
+//			try {
+//				globalProp.save();
+//			} catch (IOException ex) {
+//				ExceptionTool.showError(ex);
+//			}
+//
+//			SwingUtilities.invokeLater(new Runnable() {
+//				@Override
+//				public void run() {
+//					MainFrame.FRAME.initComponent();
+//					// CampaignClient.createClientCampaign(5, MainFrame.FRAME);
+//					MainFrame.FRAME.setVisible(true);
+//				}
+//			});
+//		} else {
+//			System.exit(0);
+//		}
 	}
 }
