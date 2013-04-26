ede0c77d87e26aa30ee0f51943bd9b0f76b97ffe
cody.stoutenburg@gmail.com <cody.stoutenburg@gmail.com@9b2beeca-6fbb-a3e4-6c1f-7d7f8f7dc6a2>
2013-04-26 01:40:05 +0000

    remove old version of the server

--- trunk/NewServer/src/main/java/net/alteiar/server/document/map/battle/character/ICharacterCombatListenerRemote.java
+++
@@ -1,8 +0,0 @@
-package net.alteiar.server.document.map.battle.character;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ICharacterCombatListenerRemote extends Remote {
-	void initiativeChanged(int newInit) throws RemoteException;
-}
